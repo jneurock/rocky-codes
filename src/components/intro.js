b66db08fe2a20ea87e327a7184e504ee72eb6798
Rocky Neurock <jneurock@gmail.com>
2021-02-10 23:17:08 +0100

    Improve color mode toggle margin and intro content

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 export default function Intro() {
   return (
-    <p className="intro">
-      <img
-        className="intro__photo"
-        alt="Rocky Neurock"
-        src="/images/rocky.jpg"
-      />
-      Hi, my name is Rocky. I built this site so I could share thoughts about
-      software engineering and leadership, write about my own personal approach
-      and share my projects. <span className="hide-sm">I love to meet new people
-      so feel free to connect with me!</span>
-    </p>
+    <section className="intro">
+      <h2 className="intro__heading">
+        Hi.
+      </h2>
+      <p>
+        <img
+          className="intro__photo"
+          alt="Rocky Neurock"
+          src="/images/rocky.jpg"
+        />
+        My name is Rocky. I built this site so I could share thoughts about
+        software engineering and leadership, write about my own personal approach
+        and share my projects. <span className="hide-sm">I love to meet new people
+        so feel free to connect with me!</span>
+      </p>
+    </section>
   );
 }
