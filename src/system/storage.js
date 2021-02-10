f72c8232b498beea3d180fa3442ba7936b807752
Rocky Neurock <jneurock@gmail.com>
2021-02-10 22:56:24 +0100

    Stub localStorage for prod build

--- src/system/storage.js
+++ src/system/storage.js
@@ -1,4 +1,15 @@
+const stubLocalStorage = () => ({
+  localStorage: {
+    getItem: () => {},
+    setItem: () => {}
+  }
+});
+
+const context = typeof window !== 'undefined'
+  ? window
+  : stubLocalStorage();
+
 export default {
-  get: key => localStorage.getItem(key),
-  set: (key, value) => localStorage.setItem(key, value)
+  get: key => context.localStorage.getItem(key),
+  set: (key, value) => context.localStorage.setItem(key, value)
 };
